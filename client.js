1763bdbcee447d4f4cc981a9f1ebb367be21ff0a
ajitalapati <ajitalapati01@gmail.com>
2021-09-02 18:05:14 -0500

    requests

--- client.js
+++ client.js
@@ -1,19 +1,35 @@
-fetch("http://localhost:3000/btcaddr", 
-{headers: {'Content-Type': 'application/json'}})
-.then(res => res.json())
-.then(data => console.log(data));
+function get(){
+    fetch("http://localhost:3000/btcaddr", 
+    {headers: {'Content-Type': 'application/json'}})
+    .then(res => res.json())
+    .then(data => console.log(data));
+}
 
-var data = {"addr": "13Js7D3q4KvfSqgKN8LpNq57gcahrVc5JZ", "sig": "H+/grsJ9K2XtbWAwAwpkypVpuRP++Grm4H+mycTUBcBbJ77CVEsU54rdSFJoJMngh0NtqDEF4tnojMd/aRH6F18="}
+function createAccount(addr, sig){
+    var data = {"addr": addr, "sig": sig}
+    fetch("http://localhost:3000/btcaddr",
+    {
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+    .then(function(res){ console.log(res) })
+    .catch(function(res){ console.log(res) });
+}
 
-
-fetch("http://localhost:3000/btcaddr",
-{
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    method: "POST",
-    body: JSON.stringify(data)
-})
-.then(function(res){ console.log(res) })
-.catch(function(res){ console.log(res) });
-
+function signIn(addr, sig){
+    var data = {"addr": addr, "sig": sig}
+    fetch("http://localhost:3000/btcaddr/login",
+    {
+        headers: {
+        'Content-Type': 'application/json'
+        },
+        method: "POST",
+        body: JSON.stringify(data)
+    })
+    .then(res => console.log(res))
+    .catch(function(res){ console.log(res) });
+    window.location.replace("http://www.w3schools.com");
+}
